597cd0f7e7d51bebdc8e9ab4edb2a313ec35d4c4
axcel0773 <yuji-watanabe1@hotmail.com>
2021-10-16 15:19:23 -0300

    Adicionar arquivos de projeto.

---
+++ ImcCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ImcCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1;
+            double n2;
+            double imc;
+
+            Console.WriteLine("Bem vindo a Calculadora de IMC");
+
+            Console.WriteLine("Digite a sua altura: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Digite o seu peso: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            imc = (n2 / Math.Pow(n1, 2));
+
+            if(imc < 20)
+            {
+                Console.WriteLine("Abaixo do peso");
+            } else if(imc >= 20 && imc < 25) {
+                Console.WriteLine("Peso ideal");
+            } else
+            {
+                Console.WriteLine("Acima do peso");
+            }
+        }
+    }
+}
